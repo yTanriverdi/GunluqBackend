b286ee065096a2931bf24aebebe02837616ab015
yasin <tanriverdi.yasinn@gmail.com>
2026-05-25 16:47:32 +0300

    Deploy sonrası swagger düzenlemesi

--- Gunluq-WebAPI/Program.cs
+++ Gunluq-WebAPI/Program.cs
@@ -107,12 +107,18 @@ namespace Gunluq_WebAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
 
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
 
             app.UseCors("gunluqPolicy");
             app.UseHttpsRedirection();
